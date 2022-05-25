75dd611ec83757f169ca00651393bc3f44b92cb0
Cell <suhail9453531504@gmail.com>
2022-05-25 13:11:33 +0530

    Part 1-Done

--- twitter/admin.py
+++ twitter/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 
-admin.site.unregister(Group)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
+    inlines = [ProfileInline]
 
 admin.site.unregister(User)  # Unregister the default User model
 admin.site.register(User, UserAdmin)  # Register the model with custom UserAdmin
+admin.site.unregister(Group)
