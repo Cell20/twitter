5b173cc72dfb9b8297dc44e5fb01ce053f4ece5a
Cell <suhail9453531504@gmail.com>
2022-05-29 23:28:01 +0530

    Reset Password Functionality

--- users/urls.py
+++ users/urls.py
@@ -17,4 +17,10 @@ urlpatterns = [
     # change password urls
     path('password_change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('users:password_change_done')), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('users:password_reset_done')), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('users:password_reset_complete')), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
